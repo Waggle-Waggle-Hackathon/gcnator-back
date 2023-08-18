b01dc70d89ae6faa52f0347ac4e768aec5deee45
joeun-01 <jjy020121@gmail.com>
2023-08-19 05:20:26 +0900

    score 가중치 계산

--- src/main/java/com/example/demo/src/question/AnswerService.java
+++ src/main/java/com/example/demo/src/question/AnswerService.java
@@ -19,7 +19,6 @@ public class AnswerService {
     private final UserRepository userRepository;
 
     public void postResult(AnswerDto answerDto, Long userId) {
-
         // 응답 중복 방지 저장용 테이블에 추가
         Answer answer = new Answer();
         Question question = questionRepository.getReferenceById(answerDto.getQuestionId());
@@ -28,18 +27,20 @@ public class AnswerService {
         answer.setUser(user);
         answer.setCategory(question.getCategory());
 
-        //유저 카테고리별 가중치 추가
+        // 유저 카테고리별 가중치 추가
         Boolean existsWeight = userWeightRepository.existsByUserIdAndCategory(userId, question.getCategory());
+        int score = question.getWeight() * answerDto.getScore();
+
         // 해당 카테고리에 대한 가중치가 이미 있다면
         if(existsWeight) {
             UserWeight userWeight = userWeightRepository.findByUserAndCategory(user, question.getCategory());
-            userWeight.addWeight(answerDto.getScore());
+            userWeight.addWeight(score);
             userWeightRepository.save(userWeight);
         }
         // 해당 카테고리 가중치가 없다면 생성해주기
         else {
             UserWeight userWeight = new UserWeight();
-            userWeight.setWeight(answerDto.getScore());
+            userWeight.setWeight(score);
             userWeight.setCategory(question.getCategory());
             userWeight.setUser(user);
             userWeightRepository.save(userWeight);
